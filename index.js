63f817ae4000bfa3cc4aef1b5db758ff09af5626
leonspok <igor9774@gmail.com>
2016-11-12 14:31:31 +0300

    Fix for icons

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var tabs = require("sdk/tabs");
 var button = buttons.ActionButton({
   id: "add-channels",
   label: "Add Channels to Irvue",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
   onClick: function(state) {
   		let currentURL = tabs.activeTab.url;
   		if (currentURL.indexOf("http://unsplash.com") == -1 && 
